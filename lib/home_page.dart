0257c1a3bc4614ad8db5565eefcb1c6b98812b8d
Santi <Santi@DESKTOP-IG9DTLS>
2025-07-01 20:03:51 -0300

    Add HomePage widget with logo, tagline, and start quiz button

---
+++ lib/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min, // Adjusts to fit the content size
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(129, 255, 255, 255),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 75.0, bottom: 25.0),
+            child: const Text(
+              "Learn flutter the fun way.",
+              style: TextStyle(fontSize: 24, color: Colors.white),
+            ),
+          ),
+          ElevatedButton.icon(
+            onPressed: () {
+              print("Working");
+            },
+            icon: Icon(Icons.arrow_right_alt, size: 35),
+            label: const Text(
+              'Start quiz',
+              style: TextStyle(fontSize: 20, color: Colors.black),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
